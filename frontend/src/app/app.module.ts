5e2dbff2fff212b299609ce66dce16aede6069c0
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2021-01-24 13:47:43 -0500

    Fixing issue with accidental inclusion of non-existant component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookstoreComponent } from './bookstore/bookstore.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BookstoreComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
