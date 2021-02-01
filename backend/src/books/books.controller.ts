ce765a5a7a8cba85c91312e57bf836ddad516b1d
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2021-01-31 21:56:45 -0500

    Finishing initial Books API resource. Adjusting Nginx file to account for rewrite rule to backend.

---
+++ backend/src/books/books.controller.ts
@@ -0,0 +1,83 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Book, Prisma } from '@prisma/client';
+
+import { BooksService } from './books.service';
+
+@Controller('books')
+export class BooksController {
+  /**
+   * BooksController constructor
+   * @param $booksService The database connection to the `books` table
+   */
+  constructor(private readonly $booksService: BooksService) {}
+
+  /**
+   * GET request to find all records in the `books` table.
+   * Routed to '/books'
+   *
+   * @param query Query parameters to alter the `WHERE` SQL clause
+   */
+  @Get()
+  async findAll(@Query() query: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.BookWhereUniqueInput;
+    where?: Prisma.BookWhereInput;
+    orderBy?: Prisma.BookOrderByInput;
+  }): Promise<Book[]> {
+    return this.$booksService.findAll(query);
+  }
+
+  /**
+   * GET request to find a book by a string UUID
+   * Routed to '/books/:id'
+   *
+   * @param id The UUID of the requested book
+   */
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Book | null> {
+    return this.$booksService.findOne({id: id} as Prisma.BookWhereUniqueInput);
+  }
+
+  /**
+   * POST request to create a new Book in the `books` table
+   * Routed to '/books'
+   *
+   * @param postData The book data to be created
+   */
+  @Post('')
+  async create(
+    @Body() postData: Prisma.BookCreateInput
+  ): Promise<Book> {
+    return this.$booksService.createBook(postData);
+  }
+
+  /**
+   * PUT request to update a Book in the `books` table
+   * Routed to '/books/:id'
+   *
+   * @param id The UUID of the book to be updated
+   * @param bookData The updated information of the Book
+   */
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() bookData: Book
+  ): Promise<Book> {
+    return this.$booksService.updateBook({
+      where: { id: id } as Prisma.BookWhereUniqueInput,
+      data: bookData
+    });
+  }
+
+  /**
+   * DELETE request to remove a Book from the `books` table
+   * Routed to '/books/:id'
+   *
+   * @param id The UUID of the Book to be removed
+   */
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Book> {
+    return this.$booksService.deleteBook({id: id} as Prisma.BookWhereUniqueInput);
+  }
+}
---
+++ backend/src/books/books.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@nestjs/common';
+import { Book, Prisma } from '@prisma/client'
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class BooksService {
+  constructor(private $prisma: PrismaService) {}
+
+  /**
+   * Find a single Book in the `books` table
+   *
+   * @param bookWhereUniqueInput Input which specifies the book to be found
+   */
+  async findOne(bookWhereUniqueInput: Prisma.BookWhereUniqueInput): Promise<Book | null> {
+    return this.$prisma.book.findUnique({
+      where: bookWhereUniqueInput
+    });
+  }
+
+  /**
+   * Finds all Book records which match the given parameters
+   *
+   * @param params Parameters to match against the `books` table entries
+   */
+  async findAll(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.BookWhereUniqueInput;
+    where?: Prisma.BookWhereInput;
+    orderBy?: Prisma.BookOrderByInput;
+  }): Promise<Book[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.$prisma.book.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy
+    });
+  }
+
+  /**
+   * Create a new Book in the `books` table
+   *
+   * @param data The Book data to be created
+   */
+  async createBook(data: Prisma.BookCreateInput): Promise<Book> {
+    return this.$prisma.book.create({
+      data
+    });
+  }
+
+  /**
+   * Updates a Book in the `books` table
+   *
+   * @param params Updated Book data
+   */
+  async updateBook(params: {
+    where: Prisma.BookWhereUniqueInput;
+    data: Prisma.BookUpdateInput;
+  }): Promise<Book> {
+    const { where, data } = params;
+    return this.$prisma.book.update({
+      data,
+      where
+    });
+  }
+
+  /**
+   * Removes a Book entry from the `books` table
+   *
+   * @param where The unique identifier(s) of the Book to be removed
+   */
+  async deleteBook(where: Prisma.BookWhereUniqueInput): Promise<Book> {
+    return this.$prisma.book.delete({
+      where
+    });
+  }
+}
