12010f742d40236d2264fa2e7d9ee92500ff0e15
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2021-02-12 19:09:58 -0500

    Fixing testing of Auth controller.

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +9,10 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ AuthController ],
+      providers: [
+        PrismaService,
+        AuthService
+      ]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
