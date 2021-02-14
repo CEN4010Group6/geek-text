4369246613d75b9000b5b97907ca335d11d60410
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2021-02-14 16:06:58 -0500

    Fixing error with Ratings and Users services.

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [ UsersController ],
-  providers: [ UsersService ],
+  providers: [ PrismaService, UsersService ],
   exports: [ UsersService ]
 })
 export class UsersModule {}
