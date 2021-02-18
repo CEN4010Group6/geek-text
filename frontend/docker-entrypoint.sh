a03a5f80d0b6d8ad69ddb8e9e0d0a9e3428ecc3d
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2021-02-17 21:13:56 -0500

    Hacking install issues with frontend container

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-if [[ $NODE_ENV == 'development' ]]; then
-  if ! diff -q ./package-lock.json /tmp/package-lock.json; then
-    echo "Updating 'node_modules'"
-    npm install --no-optional
-  fi
-fi
+# if [[ $NODE_ENV == 'development' ]]; then
+#   if ! diff -q ./package-lock.json /tmp/package-lock.json; then
+#     echo "Updating 'node_modules'"
+#     npm install --no-optional
+#   fi
+# fi
+
+npm install --no-optional
 
 if [[ $NODE_ENV == 'development' ]]; then
   ng serve --host "0.0.0.0" --disable-host-check --watch --live-reload
